7d06f4d2691661144c86442d5f64c7caa87cd4c2
neinzz <heins.nicholas@gmail.com>
2016-10-18 12:33:46 -0700

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 4f;
+
+	Rigidbody2D rbody;
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+	
+		rbody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector2 movement_vector = new Vector2(Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if (movement_vector != Vector2.zero) {
+			anim.SetBool ("iswalking", true);
+			anim.SetFloat ("input_x", movement_vector.x);
+			anim.SetFloat ("input_y", movement_vector.y);
+		} else {
+			anim.SetBool ("iswalking", false);
+		}
+
+		rbody.MovePosition (rbody.position + movement_vector * Time.deltaTime * 4);
+	}
+}
